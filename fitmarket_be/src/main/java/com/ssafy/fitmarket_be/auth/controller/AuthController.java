2867d81147c54646f5fde96c2a7656017d0dc7fa
nyeong <duddnd4457@chosun.ac.kr>
2025-12-10 17:52:18 +0900

    refactor(be): 로그인 인증 controller 구현에서 filter로 수정 (controller 삭제)

--- fitmarket_be/src/main/java/com/ssafy/fitmarket_be/auth/controller/AuthController.java
+++ fitmarket_be/src/main/java/com/ssafy/fitmarket_be/auth/controller/AuthController.java
@@ -1,70 +1,73 @@
-package com.ssafy.fitmarket_be.auth.controller;
-
-import com.ssafy.fitmarket_be.auth.CookieUtils;
-import com.ssafy.fitmarket_be.auth.dto.CustomUserDetails;
-import com.ssafy.fitmarket_be.auth.jwt.JwtUtil;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-@Slf4j
-@RequestMapping("/auth")
-public class AuthController {
-
-  private final AuthenticationManager authenticationManager;
-  private final JwtUtil jwtUtil;
-//  private final CartService cartService;
-
-@PostMapping("/login")
-public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request,
-    HttpServletResponse response) {
-  log.trace("attempt login email: {}", request.getEmail());
-
-  UsernamePasswordAuthenticationToken authToken =
-      new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
-
-  Authentication authentication = this.authenticationManager.authenticate(authToken);
-  CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
-
-  String token = this.jwtUtil.create(user.getId(), user.getUsername(), user.getAuthorities());
-  Cookie cookie = CookieUtils.create("token", token);
-  response.addCookie(cookie);
-
-  log.trace("login success email: {}, token: {}", request.getEmail(), token);
-//    int cartCount = cartService.countByUserId(user.getId());
-  int cartCount = 0;
-  return ResponseEntity.ok(new LoginResponse(user.getName(), cartCount));                                                // Changed
-}
-
-  @NoArgsConstructor
-  @Getter
-  @Setter
-  @ToString
-  public static class LoginRequest {
-    private String email;
-    private String password;
-  }
-
-  @AllArgsConstructor
-  @Getter
-  public static class LoginResponse {
-    private String name;
-    private int cartCount;
-  }
-}
+/**
+ * 나중에 참고용 ..
+ */
+//package com.ssafy.fitmarket_be.auth.controller;
+//
+//import com.ssafy.fitmarket_be.auth.CookieUtils;
+//import com.ssafy.fitmarket_be.auth.dto.CustomUserDetails;
+//import com.ssafy.fitmarket_be.auth.jwt.JwtUtil;
+//import jakarta.servlet.http.Cookie;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//@Slf4j
+//@RequestMapping("/auth")
+//public class AuthController {
+//
+//  private final AuthenticationManager authenticationManager;
+//  private final JwtUtil jwtUtil;
+////  private final CartService cartService;
+//
+//@PostMapping("/login")
+//public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request,
+//    HttpServletResponse response) {
+//  log.trace("attempt login email: {}", request.getEmail());
+//
+//  UsernamePasswordAuthenticationToken authToken =
+//      new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
+//
+//  Authentication authentication = this.authenticationManager.authenticate(authToken);
+//  CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+//
+//  String token = this.jwtUtil.create(user.getId(), user.getUsername(), user.getAuthorities());
+//  Cookie cookie = CookieUtils.create("token", token);
+//  response.addCookie(cookie);
+//
+//  log.trace("login success email: {}, token: {}", request.getEmail(), token);
+////    int cartCount = cartService.countByUserId(user.getId());
+//  int cartCount = 0;
+//  return ResponseEntity.ok(new LoginResponse(user.getName(), cartCount));                                                // Changed
+//}
+//
+//  @NoArgsConstructor
+//  @Getter
+//  @Setter
+//  @ToString
+//  public static class LoginRequest {
+//    private String email;
+//    private String password;
+//  }
+//
+//  @AllArgsConstructor
+//  @Getter
+//  public static class LoginResponse {
+//    private String name;
+//    private int cartCount;
+//  }
+//}
---
+++ fitmarket_be/src/main/java/com/ssafy/fitmarket_be/auth/filter/CustomLoginFilter.java
@@ -0,0 +1,110 @@
+package com.ssafy.fitmarket_be.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final ObjectMapper objectMapper;
+
+  /**
+   * 의존성 주입 후 경로 설정
+   *
+   * @param authenticationManager
+   */
+  public CustomLoginFilter(AuthenticationManager authenticationManager,
+      ObjectMapper objectMapper) {
+    super.setAuthenticationManager(authenticationManager);
+    this.objectMapper = objectMapper;
+
+    setFilterProcessesUrl("/auth/login");  // login uri
+  }
+
+  /**
+   * 로그인 시도
+   *
+   * @return
+   */
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request,
+      HttpServletResponse response) {
+    if (!request.getMethod().equals("POST")) { // http method check
+      throw new AuthenticationServiceException("지원하지 않는 로그인 메소드입니다: " + request.getMethod());
+    }
+
+    try {
+      // 1) JSON Body 파싱
+      LoginRequest loginRequest = this.objectMapper.readValue(request.getInputStream(),
+          LoginRequest.class);
+
+      log.trace("attempt login user email: {}", loginRequest.getEmail());
+
+      // 2) AuthenticationManager에 위임
+      return this.getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(
+          loginRequest.getEmail(), loginRequest.getPassword()));
+    } catch (IOException e) {
+      log.error("로그인 요청 JSON 파싱 실패", e);
+      throw new RuntimeException(e);
+    }
+  }
+
+//  /**
+//   * 로그인 성공 후 로직
+//   *
+//   * @param request
+//   * @param response
+//   * @param chain
+//   * @param authResult the object returned from the <tt>attemptAuthentication</tt> method.
+//   * @throws IOException
+//   * @throws ServletException
+//   */
+//  @Override
+//  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+//      FilterChain chain, Authentication authResult) {
+//    CustomUserDetails user = (CustomUserDetails) authResult.getPrincipal();
+//
+//    String token = this.jwtUtil.create(user.getId(), user.getUsername(), user.getAuthorities());
+//    Cookie cookie = CookieUtils.create("token", token);
+//
+//    log.trace("created token: {}", token);
+//
+//    response.addCookie(cookie);
+//    response.setStatus(HttpStatus.OK.value());
+//  }
+
+  static class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public LoginRequest() {
+    }
+
+    public String getEmail() {
+      return email;
+    }
+
+    public void setEmail(String email) {
+      this.email = email;
+    }
+
+    public String getPassword() {
+      return password;
+    }
+
+    public void setPassword(String password) {
+      this.password = password;
+    }
+  }
+}
